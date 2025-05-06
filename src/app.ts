7e715993d081b4c89403a94150b7a3599509e433
Briannc6 <correabrian1806@gmail.com>
2025-05-06 09:08:56 +0200

    modificando el servidor y enlaces

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { PaymentController } from './controllers/PaymentController';
 import { ContactController } from './controllers/ContactController';
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000; 
 
 app.use(express.json());
 
@@ -11,5 +11,5 @@ app.post('/contact/add', ContactController.addContact);
 app.post('/payment/add', PaymentController.addPayment);
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
